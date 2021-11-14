bbfb31e6441bce00db293f57bf70b7e68f0c7015
Carlos Barcos <fbarcos8@hotmail.com>
2021-11-14 03:02:00 -0500

    Se agrega configuracion basica para socket.io
    	modificado:     .gitignore
    	modificado:     index.js
    	modificado:     package-lock.json
    	modificado:     package.json
    	modificado:     public/index.html

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+// App express
 const app = express();
+
+// Noode server
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+// Sockets message
+io.on('connection',client=>{
+	console.log("Cliente conectado");
+	client.on('disconnect',()=>{
+		console.log("Cliente desconectado");
+	});
+});
+
 
 const publicPath = path.resolve(__dirname,'public');
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT,(err)=>{
+server.listen(process.env.PORT,(err)=>{
 	if(err) throw new Error(err);
 	console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 })
