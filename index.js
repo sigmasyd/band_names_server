aaec94d3ccfea8d3dd8320d80a2d58feb1dad57b
Carlos Barcos <fbarcos8@hotmail.com>
2021-11-17 00:35:30 -0500

    Configuracion basica para emitir y escuchar eventos
    	modificado:     index.js
    	modificado:     public/index.html

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ io.on('connection',client=>{
 	client.on('disconnect',()=>{
 		console.log("Cliente desconectado");
 	});
+	client.on('mensaje',(payload)=>{
+		console.log('Mensaje: ', payload);
+
+		io.emit('mensaje',{admin: 'nuevo mensaje'});
+
+	});
 });
 
 
